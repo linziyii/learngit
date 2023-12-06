49ef3988fc67ab4668030994e8493e85b93c9387
linziyi <ziyil8436@gmail.com>
2023-12-06 15:22:30 +0800

    text

---
+++ doc/all.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test (
+                      id BIGINT NOT NULL,
+                      name VARCHAR(30) NOT NULL,
+                      password VARCHAR(30) NOT NULL,
+                      PRIMARY KEY (id)
+) 
--- src/main/java/com/example/mysql/controller/TestController.java
+++ src/main/java/com/example/mysql/controller/TestController.java
@@ -1,4 +1,19 @@
 package com.example.mysql.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class TestController {
+    @RequestMapping("/hello/get")
+    public String hello(){
+        return "hello world";
+    }
+    @PostMapping("/hello/post")
+    public String posthello(String map){
+        return map;
+    }
 }
