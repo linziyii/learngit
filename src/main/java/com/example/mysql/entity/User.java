ed9d97e388a6d4be5aadb72bf021cd178b720ba6
linziyi <ziyil8436@gmail.com>
2023-12-06 12:28:31 +0800

    text

---
+++ src/main/java/com/example/mysql/entity/User.java
@@ -0,0 +1,51 @@
+package com.example.mysql.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.util.List;
+
+@TableName("mys")
+//方便接口找到表
+public class User {
+   @TableId(type = IdType.AUTO)
+    public int id;
+    public String Username;
+    public String password;
+    public String birthday;
+  //  @TableField(exist = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+
+}
