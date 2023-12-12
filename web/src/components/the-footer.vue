8117d6afad1ab50fc7b0e2007f7b591039217395
myy <1102366289@qq.com>
2023-12-12 15:48:33 +0800

    使用vuex保存用户信息，方便其它组件获取登录信息。遗留问题，刷新后值没有了

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,18 +1,21 @@
 <template>
-    <a-layout-footer style="text-align: center">
-      <span>MyKi ©2023</span>
-      <div class="a-item" style="display: flex; justify-content: center; gap: 10px; color: #888888;">
-        <a href="">关于我们</a>
-        <a href="">加入我们</a>
-        <a href="">意见建议</a>
-      </div>   
-    </a-layout-footer>
+  <a-layout-footer style="text-align: center">
+    甲蛙电子书，欢迎：{{user.name}}
+  </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
-import { defineComponent } from 'vue'; 
+  import { defineComponent, computed } from 'vue';
+  import store from "@/store";
+
   export default defineComponent({
-  name: 'the-footer',
-});
+    name: 'the-footer',
+    setup() {
+      const user = computed(() => store.state.user);
+
+      return {
+        user
+      }
+    }
+  });
 </script>
