794dc7b543b5de96f6676ab9b3ee472e2662e1bf
myy <1102366289@qq.com>
2023-12-18 00:55:13 +0800

    feat:优化页脚

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,7 +1,12 @@
 <template>
-  <a-layout-footer style="text-align: center">
-    甲蛙电子书<span v-show="user.id">，欢迎：{{user.name}}</span>
-  </a-layout-footer>
+ <a-layout-footer style="text-align: center">
+    <span>MyKi ©2023</span>
+      <div class="a-item" style="display: flex; justify-content: center; gap: 10px; color: #888888;">
+      <a href="">关于我们</a>
+      <a href="">加入我们</a>
+      <a href="">意见建议</a>
+    </div>
+   </a-layout-footer>
 </template>
 
 <script lang="ts">
