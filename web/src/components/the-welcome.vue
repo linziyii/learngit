ea8b6f6cc1c11ef41f334ae231ac7b71a0c5b834
myy <1102366289@qq.com>
2023-12-18 09:41:25 +0800

    feat:风格统一

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div class="tip">
+    <a-card class="tip">
       <div><b>网站说明:</b></div>
       <div>1. 统计数据是实时的，一分钟左右延时，<b>用到了定时器、复杂SQL统计、echarts</b></div>
       <div>2. 有文档被别人点赞，收到实时通知哦！<b>用到了websocket、异步化、RocketMQ、防重设计</b></div>
       <div>3. 文档树可无限级扩展，支持文字、图片、视频。<b>用到了无限级树设计知识</b></div>
       <div>4. 登录后可看到更多菜单。<b>用到了单点登录、前后端登录拦截、安全性设计</b></div>
 
-    </div>
+    </a-card>
 
     <a-row>
       <a-col :span="24">
@@ -265,7 +265,7 @@
   .tip {
     padding: 10px 5px;
     margin-bottom: 20px;
-    border: 1px solid transparent;
+    /* border: 1px solid transparent; */
     background: linear-gradient(white,white) padding-box,repeating-linear-gradient(-45deg, black 0, black 25%, white 0, white 50%) 0/.6em .6em;
     animation:ants 12s linear infinite;
   }
